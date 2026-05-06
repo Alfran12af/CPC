fd6c98c13906c74c0a30b3bc99a82f34adc53fb4
pol.jimenez.serrano <pol.jimenez.serrano@ESEIAAT-22-17>
2026-05-06 19:15:42 +0200

    Codi motor solid, ha revisar i juntar amb els altres.

--- Tsiolkovski.m
+++ Tsiolkovski.m
@@ -2,7 +2,7 @@ function I_s = Tsiolkovski(deltaV, m_r)
 % Devuelve el I_s a partir de la de DeltaV y mass ratio
 
 
-    g0 = 9.0665;                            % Sea level gravity [m/s^2]
+    g0 = 9.80665;                            % Sea level gravity [m/s^2]
     I_s = deltaV/(g0 * ln(m_r));
 
 end
